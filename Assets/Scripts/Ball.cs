03abc01dc3f19dad1c593c2933a30c9b12361577
TheTheivo <roin.neil.sullivan@gmail.com>
2018-03-10 14:05:09 +0100

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	
+    public AudioClip ball_bounce;
+	private Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddle = FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted) {
+			// Lock the ball relative to the paddle.
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// Wait for a mouse press to launch.
+			if (Input.GetMouseButtonDown(0)) {
+				print ("Mouse clicked, launch ball");
+				hasStarted = true;
+				this.GetComponent<Rigidbody2D>().velocity = new Vector2 (2f, 10f);
+			}
+		}
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+		
+		if (hasStarted) {
+            AudioSource.PlayClipAtPoint(ball_bounce, transform.position, 0.8f);
+			//GetComponent<AudioSource>().Play();
+			GetComponent<Rigidbody2D>().velocity += tweak;
+		}
+	}
+}
